bb197e5f59db7f9e90582280aee1b01e0a679094
unknown <henrytesting8@gmail.com>
2023-03-24 22:26:13 +0700

    parsing request to server l-34

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
     res.write(
-      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit' name='/message'>send</button></form></body>"
+      "<body><form action='/message' method='POST'><input type='text' name='message'/><button type='submit'>send</button></form></body>"
     );
     res.write("</html>");
     return res.end();
